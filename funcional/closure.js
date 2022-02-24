6bb616a80f2d8c8d6e1c25cbd863ffb274370fd9
ltrismegistus <trismegistolucas1@gmail.com>
2022-02-24 08:51:49 -0300

    limpeza de arquivos

--- funcional/closure.js
+++
@@ -1,8 +0,0 @@
-// Closure é quando uma função "lembra"
-// seu escopo léxico, mesmo quando a função
-// é executada fora desse escopo léxico
-
-const somarXMais3 = require('./closure_escopo')
-
-const x = 1000
-console.log(somarXMais3())
