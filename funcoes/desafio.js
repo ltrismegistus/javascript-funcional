e97d91c78093b33f4a10231fc514c98c3500f9f2
ltrismegistus <trismegistolucas1@gmail.com>
2022-02-25 17:44:11 -0300

    aula 17

--- funcoes/desafio.js
+++ funcoes/desafio.js
@@ -8,13 +8,24 @@ const somar3numeros = function(n1) {
 
 console.log(somar3numeros(1)(1)(1))
 
+function somar(a, b){
+    return a + b
+}
+function sub(a, b){
+    return a - b
+}
+function mul(a, b){
+    return a * b
+}
+function div(a, b){
+    return a / b
+}
 const calc  = function(n1) {
-    function somar(n1, n2){
-        return n1 + n2
-    }
     return function(n2) {
-        return somar(n1,n2)
+        return function(fn) {
+            return fn(n1, n2)
+        }
     }
 }
 
-console.log(calc(1)(1)(somar))
+console.log(calc(1)(0)(div))
