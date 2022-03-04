e5d4815c1f1995d0db26c7a9a8a7e3ff4bbce7d1
ltrismegistus <trismegistolucas1@gmail.com>
2022-03-04 18:07:09 -0300

    aula 18 parte 1

---
+++ funcoes/arrowFunction.js
@@ -0,0 +1,11 @@
+// arrow function
+
+const ola = () => console.log('ola')
+ola()
+
+// arrow funcition sem o return se tiver um unico 
+//parametro pode tirar os parenteses
+
+const saudacao = (nome) => "Fala" + nome + "!!!"
+const saudacao2 = nome => "Fala" + nome + "!!!"
+const saudacao3 = nome => `Fala ${nome}!!!`
